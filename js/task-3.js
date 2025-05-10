16694a62691d3443308d7d221698c80e6237151c
Valerii Riznyck <valera451reznick@gmail.com>
2025-05-10 21:08:08 +0300

    add js files

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function getElementWidth(content, padding, border) {
+    numContent = Number.parseInt(content);
+    numPadding = Number.parseInt(padding);
+    numBorder = Number.parseFloat(border);
+    
+    const totalWidth = numContent + numPadding * 2 + numBorder * 2;
+    return totalWidth
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
